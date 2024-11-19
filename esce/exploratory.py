60f49b207e949203f7fa24ac4972c1211e20d3ac
Jeremiah Lowhorn <104390957+lowhornj@users.noreply.github.com>
2024-11-18 21:08:38 -0600

    additional exploration

--- esce/exploratory.py
+++ esce/exploratory.py
@@ -7,8 +7,20 @@ This is a temporary script file.
 
 import torch
 import polars as pl
+import altair as alt
+alt.data_transformers.disable_max_rows()
+
 for i in range(torch.cuda.device_count()):
    print(torch.cuda.get_device_properties(i).name)
    
    
 cats_df = pl.read_csv("data/data.csv", separator=",")  
+
+
+alt.Chart(cats_df).transform_density(
+    'aimp',
+    as_=['aimp', 'density'],
+).mark_area().encode(
+    x="aimp:Q",
+    y='density:Q',
+)
